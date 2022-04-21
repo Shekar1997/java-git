b49330ef2433bbe501a9485cf75cb665e30772fb
shami <shekarnallamekala@gmail.com>
2022-04-21 14:38:50 +0530

    first commit....

---
+++ src/demo2/Git.java
@@ -0,0 +1,18 @@
+package demo2;
+
+public class Git {
+
+	public static void main(String[] args) {
+		
+		String hungry = "yes";
+		
+		if (hungry == "yes")
+			System.out.println(" eat somthing ....");
+		else {
+			System.out.println(" do work ....");
+	
+		}
+	}
+
+}
+
