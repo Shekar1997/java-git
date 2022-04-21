ae2fd190bdf2523ff23baa4f673ab024e81f90aa
shami <shekarnallamekala@gmail.com>
2022-04-21 16:31:49 +0530

    first commit...

---
+++ src/april221/Excel.java
@@ -0,0 +1,10 @@
+package april221;
+
+public class Excel {
+
+	public static void main(String[] args) {
+	System.out.println("my name is shekar bro");
+    System.out.println("i now eating");
+	}
+
+}
---
+++ src/demo2/Git1.java
@@ -0,0 +1,10 @@
+package demo2;
+
+public class Git1 {
+
+	public static void main(String[] args) {
+		System.out.println("my name is shekar");
+         System.out.println("i go to my room");
+	}
+
+}
