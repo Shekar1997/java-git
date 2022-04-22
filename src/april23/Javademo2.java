d99bcee466dee46f738ba743f089311beb394e41
shami <shekarnallamekala@gmail.com>
2022-04-22 10:56:06 +0530

    fouth commit..

---
+++ src/april23/Javademo2.java
@@ -0,0 +1,11 @@
+package april23;
+
+public class Javademo2 {
+
+	public static void main(String[] args) {
+		System.out.println("hi my name is rahul");
+		System.out.println("i am working wipro");
+
+	}
+
+}
